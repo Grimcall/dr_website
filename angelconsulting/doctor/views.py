9cfc5f34cb7668d71dfb631ebe3610f05ce454e9
Grimcall <mccoycross@gmail.com>
2023-06-07 18:43:41 -0400

    Added some styles, header, and routed index.

--- angelconsulting/doctor/views.py
+++ angelconsulting/doctor/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+from django.template import loader
 
-def index(request):
-    return HttpResponse("Bonjour, frére.")
+def home_template(self, request : HttpRequest):
+    nombre = request.POST.get("nombre")
+    email = request.POST.get("email")
+    mensaje = request.POST.get("mensaje")
+
+def index(request : HttpRequest):
+    return render(request, "index.html")
+
+
 # Create your views here.

f64d2d1b63a52dca327a31ec4b0e15806a9de605
Grimcall <mccoycross@gmail.com>
2023-06-15 14:56:36 -0400

    reviews section.

---
+++ angelconsulting/doctor/static/js/script.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel({
+    items: 2, // Number of items to show at a time (you can adjust this value)
+    loop: true, // Loop through the carousel
+    nav: true, // Show navigation arrows
+    dots: false // Hide navigation dots
+  });
+});
